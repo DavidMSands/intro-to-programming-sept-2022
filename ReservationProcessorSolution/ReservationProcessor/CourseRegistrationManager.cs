8b9535aed29b0f5e4f7213cfa30b0d4d0e92a77b
David Sands <davidmaxsands@gmail.com>
2022-09-12 16:46:57 -0400

    Done with Monday

---
+++ ReservationProcessorSolution/ReservationProcessor/CourseRegistrationManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class CourseRegistrationManager
+{
+
+    public CourseRegistrationResponse RegisterForCourse(string? email, string? courseId, string? courseOfferingId)
+    {
+        var registered = !email.EndsWith("aol.com");
+        return new CourseRegistrationResponse(Guid.NewGuid().ToString(), DateTime.Now.AddDays(15), registered);
+    }
+
+    public CourseRegistrationResponse RegisterForCourse(CourseRegistrationRequest request)
+    {
+        return RegisterForCourse(request.Email, request.CourseId, request.CourseOfferingId);
+    }
+}
---
+++ ReservationProcessorSolution/ReservationProcessor/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Register for a Course");
+
+//Console.Write("Enter your email: ");
+//string? email = Console.ReadLine();
+
+//Console.Write("Enter your esired Course ID: ");
+//string? courseId = Console.ReadLine();
+
+//Console.Write("Enter your Offering ID: ");
+//string? offeringId = Console.ReadLine();
+
+
+//Console.WriteLine($"Thank you for registering with email address: \"{email}\" for course {courseId} and offering {offeringId}.");
+//CourseRegistrationManager couurseRegistrationManager = new CourseRegistrationManager();
+
+//CourseRegistrationRequest request = new CourseRegistrationRequest(email, courseId, courseOfferingId);
+
+//CourseRegistrationResponse response = couurseRegistrationManager.RegisterForCourse(email, courseId, offeringId);
+
+//if(response.IsRegistered)
+//{
+//    Console.WriteLine("Congratulations! You have been registered!");
+//    Console.WriteLine($"Your Registration Id is {response.Id}");
+//    Console.WriteLine($"The course starts on {response.courseDate:d}");
+//}
+//else
+//{
+//    Console.WriteLine("Sorry, you are not registered for the course!");
+//}
+
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddScoped<CourseRegistrationManager>();
+
+var app = builder.Build();
+
+app.MapPost("/registrations", (
+    [FromBody] CourseRegistrationRequest request,
+    [FromServices] CourseRegistrationManager manager) =>
+{
+    var response = manager.RegisterForCourse(request);
+    if (response.IsRegistered)
+    {
+        return Results.Ok(response);
+    }
+    else
+    {
+        return Results.BadRequest(response);
+    }
+});
+
+
+Console.WriteLine("Fixing to run your web application!");
+app.Run();
+Console.WriteLine("Done running your web application!");
